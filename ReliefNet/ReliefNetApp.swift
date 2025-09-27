07b1bb832797ab4a5974f5e26c5e2d19f31924a5
theAyush-191 <ayush191.znr@gmail.com>
2025-09-27 22:49:31 +0530

    Initial Commit

---
+++ ReliefNet/ReliefNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliefNetApp.swift
+//  ReliefNet
+//
+//  Created by Ayush Singh on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ReliefNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
